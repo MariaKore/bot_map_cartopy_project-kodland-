c4a69d7c73759d08dc3d18e638c2f64d4b2ccd71
MariaKore <152567919+MariaKore@users.noreply.github.com>
2025-02-01 11:49:43 -0500

    Update logic.py

--- logic.py
+++ logic.py
@@ -4,49 +4,47 @@ import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import cartopy.crs as ccrs
-
+import cartopy.feature as cfeature
 
 class DB_Map():
     def __init__(self, database):
         self.database = database
-    
+
     def create_user_table(self):
         conn = sqlite3.connect(self.database)
         with conn:
             conn.execute('''CREATE TABLE IF NOT EXISTS users_cities (
                                 user_id INTEGER,
                                 city_id TEXT,
+                                marker_color TEXT,
                                 FOREIGN KEY(city_id) REFERENCES cities(id)
                             )''')
             conn.commit()
 
-    def add_city(self,user_id, city_name ):
+    def add_city(self, user_id, city_name, color='red'):
         conn = sqlite3.connect(self.database)
         with conn:
             cursor = conn.cursor()
             cursor.execute("SELECT id FROM cities WHERE city=?", (city_name,))
             city_data = cursor.fetchone()
             if city_data:
-                city_id = city_data[0]  
-                conn.execute('INSERT INTO users_cities VALUES (?, ?)', (user_id, city_id))
+                city_id = city_data[0]
+                conn.execute('INSERT INTO users_cities VALUES (?, ?, ?)', (user_id, city_id, color))
                 conn.commit()
                 return 1
             else:
                 return 0
 
-            
     def select_cities(self, user_id):
         conn = sqlite3.connect(self.database)
         with conn:
             cursor = conn.cursor()
-            cursor.execute('''SELECT cities.city 
+            cursor.execute('''SELECT cities.city, users_cities.marker_color 
                             FROM users_cities  
                             JOIN cities ON users_cities.city_id = cities.id
                             WHERE users_cities.user_id = ?''', (user_id,))
-
-            cities = [row[0] for row in cursor.fetchall()]
+            cities = [(row[0], row[1]) for row in cursor.fetchall()]
             return cities
-
 
     def get_coordinates(self, city_name):
         conn = sqlite3.connect(self.database)
@@ -62,23 +60,23 @@ class DB_Map():
         fig, ax = plt.subplots(figsize=(10, 5), subplot_kw={'projection': ccrs.PlateCarree()})
         ax.stock_img()
         ax.coastlines()
-
-        for city in cities:
+        ax.add_feature(cfeature.OCEAN, zorder=0, edgecolor='k')
+        ax.add_feature(cfeature.LAND, zorder=0, edgecolor='k')
+        ax.add_feature(cfeature.LAKES, zorder=0, edgecolor='k')
+        ax.add_feature(cfeature.RIVERS)
+        ax.add_feature(cfeature.BORDERS, linestyle=':')
+        
+        for city, color in cities:
             coordinates = self.get_coordinates(city)
             if coordinates:
                 lat, lng = coordinates
-                ax.plot(lng, lat, marker='o', markersize=5, color='red', transform=ccrs.Geodetic())
+                ax.plot(lng, lat, marker='o', markersize=5, color=color, transform=ccrs.Geodetic())
                 ax.text(lng, lat, city, transform=ccrs.Geodetic())
 
         plt.savefig(path)
         plt.close()
 
-        
-    def draw_distance(self, city1, city2):
-        pass
-
-
-if __name__=="__main__":
-    
+if __name__ == "__main__":
     m = DB_Map(DATABASE)
     m.create_user_table()
+
